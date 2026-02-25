3778bc8cc4ee4462c80be3c4b585548ec7f84218
eyesvondi <aralid.a41@gmail.com>
2026-02-25 01:01:16 +0100

    création controllers et config dans web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MotsController;
+use App\Http\Controllers\JoueurController;
+use App\Http\Controllers\PartieController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('mots', MotsController::class);
+Route::resource('joueurs', JoueurController::class);
+Route::resource('parties', PartieController::class);
+Route::resource('users', UserController::class);
